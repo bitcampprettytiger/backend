ec6a31adbf9e643014f94fad561406bc9af5cdf2
bong94688 <bong94688@gmail.com>
2023-08-04 12:09:45 +0900

    feat-25 :"JWT 로그인 기능" "가입 기능 추가"

--- src/main/java/com/example/bitcamptiger/dto/ResponseDTO.java
+++ src/main/java/com/example/bitcamptiger/dto/ResponseDTO.java
@@ -1,13 +1,14 @@
 package com.example.bitcamptiger.dto;
 
 import lombok.Data;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 @Data
 public class ResponseDTO<T> {
 
-    private List<T> items;
+    private Page<T> items;
 
     private T item;
 

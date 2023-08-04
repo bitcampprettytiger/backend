53a05d9326c2d731785fdfb482fe3f8c7efac24b
redwolf2023 <wjdtn8046@naver.com>
2023-08-04 16:34:04 +0900

    git commit -m "Feat-3:도로점용허가 api 검증 기능"

---
+++ src/main/java/com/example/bitcamptiger/RoadOccupancyCertificate/controller/RoadOcuuCertiController.java
@@ -0,0 +1,40 @@
+package com.example.bitcamptiger.RoadOccupancyCertificate.controller;
+
+import com.example.bitcamptiger.RoadOccupancyCertificate.dto.RoadOcuuCertiData;
+import com.example.bitcamptiger.RoadOccupancyCertificate.service.RoadOccuCertiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RoadOcuuCertiController {
+    @Autowired
+    private RoadOccuCertiService openApiService;
+
+    @GetMapping("/api/data")
+    public ResponseEntity<List<RoadOcuuCertiData>> fetchDataFromExternalApi() {
+        List<RoadOcuuCertiData> dataList = openApiService.fetchDataFromExternalApi();
+
+        if (dataList != null && !dataList.isEmpty()) {
+            return ResponseEntity.ok(dataList);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/api/data/{perNo}")
+    public ResponseEntity<List<RoadOcuuCertiData>> searchDataByPerNo(@PathVariable String perNo) {
+        List<RoadOcuuCertiData> dataList = openApiService.fetchDataFromExternalApi();
+        List<RoadOcuuCertiData> result = openApiService.findByPerNo(dataList, perNo);
+
+        if (!result.isEmpty()) {
+            return ResponseEntity.ok(result);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}

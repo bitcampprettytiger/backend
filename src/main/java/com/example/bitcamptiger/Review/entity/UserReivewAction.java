7517db38bcb5fbfad81808cbc62bb1f0f5fe9f60
redwolf2023 <wjdtn8046@naver.com>
2023-08-21 17:37:26 +0900

    feat-26:"좋아요 싫어요 기능 구현"

---
+++ src/main/java/com/example/bitcamptiger/Review/entity/UserReivewAction.java
@@ -0,0 +1,2 @@
+package com.example.bitcamptiger.Review.entity;public class UserReivewAction {
+}
---
+++ src/main/java/com/example/bitcamptiger/Review/repository/UserReviewActionRepository.java
@@ -0,0 +1,2 @@
+package com.example.bitcamptiger.Review.repository;public interface UserReviewActionRepository {
+}

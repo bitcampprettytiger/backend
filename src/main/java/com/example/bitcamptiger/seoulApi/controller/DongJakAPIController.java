889c732253d5109543c1155fa5a3b4c427268a8c
Youngeunlee98 <helen613@naver.com>
2023-08-23 19:48:17 +0900

    구현예정

---
+++ src/main/java/com/example/bitcamptiger/seoulApi/controller/DongJakAPIController.java
@@ -0,0 +1,11 @@
+package com.example.bitcamptiger.seoulApi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/DongJak")
+public class DongJakAPIController {
+
+
+}

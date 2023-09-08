0b664540653880c4426d799a73388358973f1ed5
bong94688 <bong94688@gmail.com>
2023-09-08 18:38:56 +0900

    fix -85: 스웨거 수정

--- src/main/java/com/example/bitcamptiger/vendor/service/Impl/VendorServiceImpl.java
+++ src/main/java/com/example/bitcamptiger/vendor/service/Impl/VendorServiceImpl.java
@@ -101,8 +101,8 @@ public class VendorServiceImpl implements VendorService {
         }
         List<Vendor> byrandmart = vendorRepository.findByrandmart(nowLocationDto);
         List<VendorDTO> vendorDTOList = new ArrayList<>();
-        List<VendorImageDTO> vendorImageDTOList = new ArrayList<>();
         for(Vendor vendor:byrandmart){
+            List<VendorImageDTO> vendorImageDTOList = new ArrayList<>();
             List<VendorImage> byVendor = vendorImageRepository.findByVendor(vendor);
             for(VendorImage vendorImage: byVendor){
                 String geturl = s3UploadService.geturl(vendorImage.getUrl() + vendorImage.getFileName());
@@ -112,8 +112,8 @@ public class VendorServiceImpl implements VendorService {
             }
 
             VendorDTO of = VendorDTO.of(vendor);
-            of.setPrimaryimgurl(vendorImageDTOList.get(0).getUrl());
             of.setVendorImageDTOList(vendorImageDTOList);
+            of.setPrimaryimgurl(vendorImageDTOList.get(0).getUrl());
             vendorDTOList.add(of);
         }
 
